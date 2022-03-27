3957a9cc9d2e4cd8f64003605d951907d57d02c5
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2022-03-27 16:38:29 +0300

    Singleton pattern

---
+++ SingletonPattern/SingletonDataContainer.cs
@@ -0,0 +1,31 @@
+﻿namespace SingletonPattern
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class SingletonDataContainer : ISingletonContainer
+    {
+        private Dictionary<string, int> capitals;
+
+        private SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing singleton object");
+            this.capitals = new Dictionary<string, int>();
+            var dataset = File.ReadAllLines("capitals.txt");
+            for (int i = 0; i < dataset.Length; i += 2)
+            {
+                capitals.Add(dataset[i], int.Parse(dataset[i + 1]));
+            }
+        }
+
+        public int GetPopulation(string name)
+        {
+            return this.capitals[name];
+        }
+
+        private static SingletonDataContainer instance = new SingletonDataContainer();
+
+        public static SingletonDataContainer Instance => instance;
+    }
+}
